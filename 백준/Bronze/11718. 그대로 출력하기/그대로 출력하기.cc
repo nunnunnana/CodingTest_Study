d93fae44eea56b230349c9d33f2fa14b13914b7a
Whitemind109 <ganada38@naver.com>
2025-04-11 16:54:17 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    
+    while(true){
+        getline(cin, s);
+        if(s == "") break;
+        
+        cout << s << '\n';
+    }
+    return 0;
+}
