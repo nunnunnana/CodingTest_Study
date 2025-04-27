be317c9c3598c36330031e4b7026fe65d210d350
Whitemind109 <ganada38@naver.com>
2025-04-27 14:52:18 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,44 +1,34 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
 int main()
 {
-    int alphabet[26] = {0, };
-    string input;
-    cin >> input;
+    int alphabet[26] = {}, odd = 0;
+    string s, left;
+    char mid = 0;
 
-    for (char c : input) alphabet[c - 'A']++;
-
-    int oddCount = 0;
-    char midChar = 0;
-    for (int i = 0; i < 26; i++) {
-        if (alphabet[i] % 2 == 1) {
-            oddCount++;
-            midChar = 'A' + i;
+    cin >> s;
+    for(char c : s) alphabet[c - 'A']++;
+        
+    for(int i = 0; i < 26; i++) {
+        if (alphabet[i] % 2) {
+            odd++;
+            mid = 'A' + i;
+            if(odd > 1) {
+                cout << "I'm Sorry Hansoo";
+                return 0;
+            }    
         }
-    }
-
-    if (oddCount > 1) {
-        cout << "I'm Sorry Hansoo";
-        return 0;
-    }
-
-    string left = "";
-    string right = "";
-
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < alphabet[i] / 2; j++) {
-            left += ('A' + i);
-        }
+        left.append(alphabet[i] / 2, i + 'A');
     }
     
-    right = left;
+    string right = left;
     reverse(right.begin(), right.end());
+    
 
-    if (midChar) cout << left << midChar << right;
-    else cout << left + right;
+    if(mid) cout << left << mid << right;
+    else cout << left << right;
 
-    return 0;
+    return 0;   
 }
