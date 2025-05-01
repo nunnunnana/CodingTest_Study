6ab6fff9bf5a4b9b265e55c003172157b08a70c7
Whitemind109 <ganada38@naver.com>
2025-05-02 01:42:12 +0900

    [Silver III] Title: 달팽이, Time: 116 ms, Memory: 5800 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.cc
+++ 백준/Silver/1913. 달팽이/달팽이.cc
@@ -4,18 +4,17 @@ using namespace std;
 int main()
 {
     int n, m;
-    
     cin >> n >> m;
-    
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, 1, 0, -1};
 
-    int arrGraph[n][n] = {};
+    int arr[n][n] = {};
+    int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+
     int x = n / 2, y = n / 2;
-    arrGraph[x][y] = 1;
+    arr[x][y] = 1;
     int a = 2;
     int step = 1;
-    int targetX = x, targetY = y;
+    int tx = x, ty = y;
+    
     
     while(a <= n * n){
         for(int i = 0; i < 4; i++){
@@ -23,24 +22,23 @@ int main()
                 x += dx[i];
                 y += dy[i];
                 if(a == m){
-                    targetX = x;
-                    targetY = y;
+                    tx = x;
+                    ty = y;
                 }
-                arrGraph[x][y] = a;
+                arr[x][y] = a;
                 a++;
-                if (a > n * n) break;
+                if(a > n * n) break;
             }
-            if (a > n * n) break;
-            if(i == 1 || i == 3) step++;
+            if(a > n * n) break;
+            if(i % 2) step++;
         }
     }
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << arrGraph[i][j] << " ";
-        }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            cout << arr[i][j] << ' ';
         cout << '\n';
     }
-    cout << targetX + 1 << " " << targetY + 1;
+    cout << tx + 1 << " " << ty + 1;
     return 0;
 }
