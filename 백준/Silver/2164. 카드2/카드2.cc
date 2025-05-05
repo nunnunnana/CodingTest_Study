83afc4040fc1cdcf35de46d474fed794e27e4931
Whitemind109 <ganada38@naver.com>
2025-05-06 00:02:24 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -2,21 +2,19 @@
 #include <queue>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
 
-    queue<int> queue;
-    for(int i = 0; i < n; i++){
-        queue.push(i);
+    queue<int> q;
+    for (int i = 1; i <= n; i++) q.push(i);
+
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
     }
-    while(queue.size() > 1){
-        queue.pop();
-        queue.push(queue.front());
-        queue.pop();
-    }
-    
-    cout << queue.front() + 1;
+
+    cout << q.front();
     return 0;
 }
