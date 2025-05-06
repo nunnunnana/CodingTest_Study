ff3c9023511dc67ecd15fdaa20de7caf92c4df03
Whitemind109 <ganada38@naver.com>
2025-05-06 15:06:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+    for (int i = 0; i < n; i++) {
+        int m;
+	    cin >> m;
+	    
+        unordered_map<string, int> clothes;
+        for (int j = 0; j < m; j++) {
+            string name, type;
+            cin >> name >> type;
+            
+            clothes[type]++;
+        }
+        
+        int result = 1;
+
+        for (const auto [x, y] : clothes) {
+            result *= y + 1;
+        }
+        
+        cout << result - 1 << '\n';
+    }
+	return 0;
+}
