918b3365a2e26df840588798ea8fcbae5652bc85
Whitemind109 <ganada38@naver.com>
2025-05-13 01:18:15 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -1,34 +1,28 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
 using namespace std;
-
-int isPrime(int n, int m){
-    int count = 0;
-    vector<bool> arr(n + 1, false);
-    for(int i = 2; i <= n; i++){
-        if(arr[i] == false){
-            for(int j = 1; i * j <= n; j++){
-                if(arr[i * j] == false){
-                    count++;
-                    arr[i * j] = true;
-                    if(count == m){
-                        cout << i * j << '\n';
-                        return 0;
-                    }
-                }
-            }
-        }
-    }
-    return 0;
-}
-
 
 int main()
 {
     int n, k;
     cin >> n >> k;
-    isPrime(n, k);
+    
+    vector<bool> isPrime(n + 1, false);
+    int count = 0;
+    
+    for(int i = 2; i <= n; i++){
+        if(isPrime[i]) continue;
+        for(int j = i; j <= n; j += i){
+            if(!isPrime[j]){
+                isPrime[j] = true;
+                count++;
+                if(count == k){
+                    cout << j << '\n';
+                    return 0;
+                }
+            }
+        }
+    }
 
     return 0;
 }
