a18e3e27c9619581d921f582841f204c7df03213
Whitemind109 <ganada38@naver.com>
2025-05-19 01:14:20 +0900

    [Silver I] Title: 피아노 체조, Time: 36 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.cc
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.cc
@@ -4,31 +4,24 @@ using namespace std;
 
 int main()
 {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     
     int n;
     cin >> n;
     
-    vector<int> a(n + 1);
-    vector<int> s(n + 1);
+    vector<int> a(n + 1), s(n + 1);
     for(int i = 1; i <= n; i++){
         cin >> a[i];
-        
-        if(a[i - 1] > a[i]) s[i] = s[i - 1] + 1;
-        else s[i] = s[i - 1];
+        s[i] = s[i - 1] + (a[i - 1] > a[i]);
     }
     
     int q;
     cin >> q;
-    
     for(int i = 0; i < q; i++){
         int x, y;
         cin >> x >> y;
-        
-        int result = s[y] - s[x];
-        cout << result << '\n';
+        cout << s[y] - s[x] << '\n';
     }
 
     return 0;
