6b26e42c3e99d7e2a5aa93efc9dedb0e3decb344
Whitemind109 <ganada38@naver.com>
2025-06-01 23:42:53 +0900

    [Silver V] Title: 배열 합치기, Time: 424 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int n, m;
     cin >> n >> m;
     
-    vector<int> s(n+m);
-    for(int i = 0; i < s.size(); i++){
-        cin >> s[i];
-    }
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
 
-    sort(s.begin(), s.end());
-    for(int i = 0; i < s.size(); i++){
-        cout << s[i] << ' ';
+    vector<int> result;
+    int i = 0, j = 0;
+
+    while(i < n && j < m) {
+        if(a[i] < b[j]) result.push_back(a[i++]);
+        else result.push_back(b[j++]);
     }
+    
+    while (i < n) result.push_back(a[i++]);
+    while (j < m) result.push_back(b[j++]);
+
+    for (int num : result) cout << num << ' ';
+    
     return 0;
 }
