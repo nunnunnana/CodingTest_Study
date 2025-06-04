64208107c8470d780e7b676984ff4c4b9355c64a
Whitemind109 <ganada38@naver.com>
2025-06-04 23:36:54 +0900

    [Silver III] Title: 수열, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -7,27 +7,22 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n, x;
-    cin >> n >> x;
+    int n, k;
+    cin >> n >> k;
     
     vector<int> a(n);
     for(int& i : a) cin >> i;
     
-    int sum = 0, count = 1;
-    for(int i = 0; i < x; i++) sum += a[i];
+    int sum = 0;
+    for(int i = 0; i < k; i++) sum += a[i];
     
     int max = sum;
-    for(int i = x; i < n; i++){
-        sum += a[i] - a[i - x];
-        
-        if(sum > max) {
-            max = sum;
-            count = 1;
-        }
-        else if(sum == max) count++;
+    for(int i = k; i < n; i++){
+        sum += a[i] - a[i - k];
+        if(sum > max) max = sum;
     }
     
-    cout << max << '\n';
+    cout << max;
 
     return 0;
 }
