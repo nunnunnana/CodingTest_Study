8213f916a9145dd143ab9dff049f79146f005a31
Whitemind109 <ganada38@naver.com>
2025-06-08 23:35:15 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s, result = "";
+    cin >> s;
+    
+    int count = 0;
+    for(int i = 0; i <= s.size(); i++){
+        if(s[i] != 'X'){
+            if (count % 2 != 0) {
+                cout << -1 << '\n';
+                return 0;
+            }
+            result += string(count / 4 * 4, 'A');
+            result += string((count % 4) / 2 * 2, 'B');
+            count = 0;
+        }
+        if(s[i] == '.') result += '.';
+        else count++;
+    }
+    cout << result << '\n';
+    return 0;
+}
