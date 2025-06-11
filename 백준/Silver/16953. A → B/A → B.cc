f155a526ac94557bc527e23c065172a82358f721
Whitemind109 <ganada38@naver.com>
2025-06-12 00:28:30 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -6,31 +6,14 @@ int main()
     int a, b;
     cin >> a >> b;
     
-    int result = b, count = 1;
-    bool check;
-    while(result > a){
-        check = true;
-        if(result % 2 == 0){
-            result /= 2;
-            count++;
-            check = false;
-        }
-        else if(to_string(result).back() == '1'){
-            string s = to_string(result);
-            s.pop_back();
-            result = stoi(s);
-            count++;
-            check = false;
-        }
-        if(check) {
-            cout << -1;
-            return 0;
-        }
+    int count = 1;
+    while(b > a){
+        if(b % 2 == 0) b /= 2;
+        else if(b % 10 == 1) b /= 10;
+        else break;
+        count++;
     }
-    if(check || result < a){
-        cout << -1;
-        return 0;
-    }
-    cout << count;
+
+    cout << ((b == a) ? count : -1);
     return 0;
 }
