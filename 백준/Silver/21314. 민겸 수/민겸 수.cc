1aeff23c42717cf5082b51fe06deefcd57bd6951
Whitemind109 <ganada38@naver.com>
2025-06-12 13:02:22 +0900

    [Silver I] Title: 민겸 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.cc
+++ 백준/Silver/21314. 민겸 수/민겸 수.cc
@@ -1,41 +1,34 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
 using namespace std;
 
 int main()
 {
     string n;
     cin >> n;
+    
+    string min, max;
     int count = 0;
-    vector<long long> min, max;
-    for(int i = 0; i <= n.size(); i++){
-        if(n[i] == 'M'){
-            if(n[i - 1] == 'M'){
-                min.push_back(0);
+    for(const char c : n){
+        if(c == 'M') count++;
+        else{
+            if(count > 0){
+                min += '1';
+                min.append(count - 1, '0');
+                max += '5';
+                max.append(count, '0');
             }
-            else{
-                min.push_back(1);
-            }
-            count++;
-            if(i == n.size() - 1) {
-                for(int j = 0; j < count; j++){
-                    max.push_back(1);
-                }
-            }
-        }
-        else if(n[i] == 'K'){
-            min.push_back(5);
-            max.push_back(5 * pow(10, count));
+            else max += '5';
+            min += '5';
             count = 0;
         }
     }
-    for(const long long i : max){
-        cout << i;
+    
+    if(count > 0){
+        max.append(count, '1');
+        min += '1';
+        min.append(count - 1, '0');
     }
-    cout << '\n';
-    for(const long long i : min){
-        cout << i;
-    }
+    
+    cout << max << '\n' << min;
     return 0;
 }
