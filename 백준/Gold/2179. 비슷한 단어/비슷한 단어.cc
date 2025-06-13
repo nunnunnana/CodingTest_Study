c73e86dc091b99e1988a7677f5e8e987abe39310
Whitemind109 <ganada38@naver.com>
2025-06-14 01:05:36 +0900

    [Gold IV] Title: 비슷한 단어, Time: 288 ms, Memory: 2652 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-int commonPrefixLength(const string& a, const string& b) {
+int Compare(const string& a, const string& b) {
     int len = min(a.size(), b.size());
     int i = 0;
     while (i < len && a[i] == b[i]) {
@@ -12,32 +11,30 @@ int commonPrefixLength(const string& a, const string& b) {
     return i;
 }
 
-int main() {
-    int N;
-    cin >> N;
-    vector<string> words(N);
+int main()
+{
+    int n;
+    cin >> n;
     
-    for (int i = 0; i < N; ++i) {
-        cin >> words[i];
+    vector<string> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
     }
 
-    int maxPrefixLen = -1;
-    int first = -1, second = -1;
-
-    // 모든 쌍 비교 (브루트포스)
-    for (int i = 0; i < N; ++i) {
-        for (int j = i + 1; j < N; ++j) {
-            int prefixLen = commonPrefixLength(words[i], words[j]);
-
-            if (prefixLen > maxPrefixLen) {
-                maxPrefixLen = prefixLen;
-                first = i;
-                second = j;
+    int max = -1, x = 0, y = 1;
+    
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int len = Compare(v[i], v[j]);
+            if (len > max) {
+                max = len;
+                x = i;
+                y = j;
             }
         }
     }
 
-    cout << words[first] << '\n' << words[second] << '\n';
-
+    cout << v[x] << '\n' << v[y] << '\n';
     return 0;
 }
+
