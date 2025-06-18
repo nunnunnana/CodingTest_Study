a6a8c8a11a8bd561b7070c6591842d2c259e1222
Whitemind109 <ganada38@naver.com>
2025-06-19 00:35:05 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -2,34 +2,33 @@
 #include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
-
-    vector<int> taller_count(n); // input: 키 1~n인 사람들의 기억
-    for (int i = 0; i < n; i++) {
-        cin >> taller_count[i];
+    
+    vector<int> v(n);
+    for(int& i : v){
+        cin >> i;
     }
-
-    vector<int> result(n, 0); // 최종 줄
-
-    for (int i = 0; i < n; i++) {
-        int count = taller_count[i]; // 내 왼쪽에 있어야 할 큰 사람 수
-        int pos = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (result[j] == 0) { // 빈 자리일 때만 count 감소
-                if (count == 0) {
-                    result[j] = i + 1; // 키는 i+1
+    
+    vector<int> result(n, 0);
+    for(int i = 0; i < n; i++){
+        int count = v[i];
+        
+        for(int j = 0; j < n; j++) {
+            if(result[j] == 0) { 
+                if(count == 0) {
+                    result[j] = i + 1;
                     break;
                 }
                 count--;
             }
         }
     }
-
-    for (int k : result) {
-        cout << k << ' ';
+    
+    for(const int i : result){
+        cout << i << ' ';
     }
 
     return 0;
