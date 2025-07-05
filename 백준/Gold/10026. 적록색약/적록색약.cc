6da8a4377388964518da1d0e34769efaab4b3c47
Whitemind109 <ganada38@naver.com>
2025-07-06 00:04:06 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2096 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
 using namespace std;
 
 vector<vector<int>> area;
@@ -10,37 +9,31 @@ int n;
 int dx[4] = {-1, 1, 0, 0}; 
 int dy[4] = {0, 0, -1, 1};
 
-int dfs(int x, int y, int color, bool colorBlind){
+void dfs(int x, int y, int color, bool colorBlind){
     visited[x][y] = true;
-    int count = 1;
+
     for(int i = 0; i < 4; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
         
-        if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-            if(!visited[nx][ny]){
-                if(colorBlind){
-                    if ((abs(color) == 1 && abs(area[nx][ny]) == 1) || (color == 2 && area[nx][ny] == 2)) {
-                        count += dfs(nx, ny, area[nx][ny], colorBlind);
-                    }
-                } 
-                else{
-                    if(area[nx][ny] == color){
-                        count += dfs(nx, ny, color, colorBlind);
-                    }
-                }
-            }
+        if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+        if(visited[nx][ny]) continue;
+        
+        if(colorBlind){
+            if(abs(area[nx][ny]) == abs(color)) 
+                dfs(nx, ny, abs(color), colorBlind);
+        }
+        else{
+            if(area[nx][ny] == color) 
+                dfs(nx, ny, color, colorBlind);
         }
     }
-    
-    return count;
 }
 
 int countRegions(bool colorBlind) {
-    visited = vector<vector<bool>>(n, vector<bool>(n, false));
+    visited.assign(n, vector<bool>(n, false));
     int count = 0;
 
-    vector<int> groupCounts;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             if (!visited[i][j]) {
@@ -52,26 +45,17 @@ int countRegions(bool colorBlind) {
     return count;
 }
 
-
-int main()
-{
+int main() {
     cin >> n;
-    
     
     area.resize(n, vector<int>(n));
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             char c;
             cin >> c;
-            if(c == 'R'){
-                area[i][j] = 1;
-            }
-            else if(c == 'G'){
-                area[i][j] = -1;
-            }
-            else if(c == 'B'){
-                area[i][j] = 2;
-            }
+            if(c == 'R') area[i][j] = 1;
+            else if(c == 'G') area[i][j] = -1;
+            else if(c == 'B') area[i][j] = 2;
         }
     }
     
@@ -79,6 +63,5 @@ int main()
     int colorBlind = countRegions(true);
 
     cout << normal << " " << colorBlind << "\n";
-
     return 0;
 }
