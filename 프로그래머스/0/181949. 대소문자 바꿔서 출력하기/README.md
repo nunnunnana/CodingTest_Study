61f811431b17571aa24135dd8d5abdd53316a76d
Whitemind109 <ganada38@naver.com>
2025-08-28 23:58:20 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 49.96 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대소문자 바꿔서 출력하기 - 181949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=javascript) 
 
 ### 성능 요약
 
-메모리: 69.2 MB, 시간: 212.58 ms
+메모리: 32.3 MB, 시간: 49.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 28일 23:35:18
+2025년 08월 28일 23:58:17
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    let result = "";
+    
+    for(let ch of str){
+        if(ch === ch.toUpperCase())
+            result += ch.toLowerCase();
+        else
+            result += ch.toUpperCase();
+    }
+    
+    console.log(result);
+});
