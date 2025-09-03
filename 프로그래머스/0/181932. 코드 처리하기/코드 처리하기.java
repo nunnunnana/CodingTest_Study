163f312cd9d969cf6a872c3cbd655dfbaa43fa6e
Whitemind109 <ganada38@naver.com>
2025-09-03 20:24:43 +0900

    [level 0] Title: 코드 처리하기, Time: 8.47 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,23 +1,17 @@
 class Solution {
     public String solution(String code) {
-        boolean mode = false;
-        String ret = "";
+        int mode = 0;
+        StringBuilder ret = new StringBuilder();
         for(int i = 0; i < code.length(); i++){
             char c = code.charAt(i);
-            if(!mode){
-                if(c != '1' && i % 2 == 0){
-                    ret += code.charAt(i);
-                }
-                else if(c == '1') mode = true;
+            if(c == '1'){
+                mode = mode == 0 ? 1 : 0;
+                continue;
             }
-            else{
-                if(c != '1' && i % 2 == 1){
-                    ret += code.charAt(i);
-                }
-                else if(c == '1') mode = false;
+            if(i % 2 == mode){
+                ret.append(c);
             }
         }
-        if(ret.isEmpty()) ret = "EMPTY";
-        return ret;
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
     }
 }
