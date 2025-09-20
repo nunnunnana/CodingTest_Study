009454a4a55f596caded123a5eca59c3b8c56d00
Whitemind109 <ganada38@naver.com>
2025-09-20 12:36:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -4,16 +4,13 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
     int count = 0;
-    for(int i = 1; i <= n; i++){
-        for(int j = i; j <= n; j++){
-            answer += j;
-            if(answer > n){
-                answer = 0;
-                break;
-            }
-            else if(answer == n){
+    for (int i = 1; i <= n; i++) {
+        int sum = 0; 
+        for (int j = i; j <= n; j++) {
+            sum += j;
+            if (sum > n) break;
+            if (sum == n) {
                 count++;
                 break;
             }
