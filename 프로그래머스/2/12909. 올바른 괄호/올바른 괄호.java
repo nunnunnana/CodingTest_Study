4e4ba10c6e5d08d084fa49ccdc67f10138983714
Whitemind109 <ganada38@naver.com>
2025-09-27 22:44:50 +0900

    [level 2] Title: 올바른 괄호, Time: 20.80 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ')'){
+                if(stack.empty()) return false;
+                else stack.pop();
+            }
+            else stack.push(s.charAt(i));
+        }
+        return stack.empty() ? true : false;
+    }
+}
