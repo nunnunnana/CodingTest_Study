c52a73fd83aec4e7283677acc60383648168535b
Whitemind109 <ganada38@naver.com>
2025-09-29 15:10:44 +0900

    [level 0] Title: 세로 읽기, Time: 2.97 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for(int i = 0; i < my_string.length() / m; i++){
+            answer += Character.toString(my_string.charAt((i * m) + c - 1));
+        }
+        return answer;
+    }
+}
