8142bb7553196753c7f4623142d4bc74f794f221
Whitemind109 <ganada38@naver.com>
2025-09-30 23:34:05 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n / k];
+        int index = 0;
+        for(int i = k; i <= n; i += k){
+            answer[index++] = i;
+        }
+        return answer;
+    }
+}
