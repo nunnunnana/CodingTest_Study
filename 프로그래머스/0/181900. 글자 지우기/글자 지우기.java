e83a21b8a2ca1e61ca4d0eeb529dde1c40387182
Whitemind109 <ganada38@naver.com>
2025-10-02 21:09:57 +0900

    [level 0] Title: 글자 지우기, Time: 18.84 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        Arrays.sort(indices);
+        int idx = 0;
+        for(int i = 0; i < my_string.length(); i++){
+            if(indices.length > idx){
+                if(indices[idx] == i){
+                    idx++;
+                    continue;
+                }
+            }
+            answer += my_string.charAt(i);
+        }    
+        return answer;
+    }
+}
