c1f82442eb2dea17f4f14dff08e5e772244e529f
Whitemind109 <ganada38@naver.com>
2025-10-03 22:19:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num - end_num + 1];
-        int count = start_num;
         for(int i = 0; i <= start_num - end_num; i++){
-            answer[i] = count--;
+            answer[i] = start_num - i;
         }
         return answer;
     }
