3a67858f7ead0c3ac14734e2e154d68744cde05b
Whitemind109 <ganada38@naver.com>
2025-10-05 00:05:28 +0900

    [level 0] Title: 리스트 자르기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=javascript) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 04일 23:58:06
+2025년 10월 05일 00:05:26
 
 ### 문제 설명
 
--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,27 +1,14 @@
 function solution(n, slicer, num_list) {
-    let answer = [];
-    const a = slicer[0]
-    const b = slicer[1]
-    const c = slicer[2]
-        if(n == 1){
-            for(let i = 0; i <= b; i++){
-                answer.push(num_list[i]);
-            }
-        }
-        else if(n == 2){
-            for(let i = a; i < num_list.length; i++){
-                answer.push(num_list[i]);
-            }
-        }
-        else if(n == 3){
-            for(let i = a; i <= b; i++){
-                answer.push(num_list[i]);
-            }
-        }
-        else {
-            for(let i = a; i <= b; i += c){
-                answer.push(num_list[i]);
-            }    
-        }
-    return answer;
+    let [a, b, c] = [...slicer];
+
+    switch(n) {
+        case 1:
+            return num_list.slice(0, b + 1)
+        case 2:
+            return num_list.slice(a, num_list.length)
+        case 3:
+            return num_list.slice(a, b + 1)
+        case 4:
+            return num_list.slice(a, b + 1).filter((_, idx) => !(idx % c));
+    }
 }
