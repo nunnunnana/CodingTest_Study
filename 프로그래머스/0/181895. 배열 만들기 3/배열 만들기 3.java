bf71189a5d2f75cd832204f62212241230467a42
Whitemind109 <ganada38@naver.com>
2025-10-07 14:33:23 +0900

    [level 0] Title: 배열 만들기 3, Time: 11.53 ms, Memory: 93.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < intervals.length; i++){
+            for(int j = intervals[i][0]; j <= intervals[i][1]; j++){
+                list.add(arr[j]);
+            }
+        }       
+        int[] answer = new int[list.size()];
+        for(int i = 0 ; i < list.size() ; i++){
+            answer[i] = list.get(i).intValue();
+        }
+        return answer;
+    }
+}
