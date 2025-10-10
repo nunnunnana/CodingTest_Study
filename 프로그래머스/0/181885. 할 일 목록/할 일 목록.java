a29288ae5db2eeee98dee52154f938de1bca7eee
Whitemind109 <ganada38@naver.com>
2025-10-10 15:49:17 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < todo_list.length; i ++){
+            if(!finished[i]) list.add(todo_list[i]);
+        }
+        String[] answer = list.toArray(new String[0]);
+        return answer;
+    }
+}
