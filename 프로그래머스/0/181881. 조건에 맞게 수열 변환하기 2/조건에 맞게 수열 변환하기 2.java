a92b11806fca7f26dacd054b75a7bd8406ae43a5
Whitemind109 <ganada38@naver.com>
2025-10-13 11:37:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 70.42 ms, Memory: 165 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr) {
+        int count = 0;
+        int[] temp = {};
+        while(!Arrays.equals(arr, temp)){
+            temp = Arrays.copyOf(arr, arr.length);
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] >= 50 && arr[i] % 2 == 0){
+                    arr[i] /= 2;
+                }
+                else if(arr[i] < 50 && arr[i] % 2 != 0){
+                    arr[i] = arr[i] * 2 + 1;
+                }
+            }
+            count++;
+        }
+
+        return count - 1;
+    }
+}
