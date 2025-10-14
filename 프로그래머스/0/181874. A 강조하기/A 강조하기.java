93a1cb99d7324456489df3dc0013919add70d8fd
Whitemind109 <ganada38@naver.com>
2025-10-14 10:32:47 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,7 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        myString = myString.toLowerCase();
-        myString = myString.replace("a", "A");
-        return myString;
+        return myString.toLowerCase().replace("a", "A");
     }
 }
