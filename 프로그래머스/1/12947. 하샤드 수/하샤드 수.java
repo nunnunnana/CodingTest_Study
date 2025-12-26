f4bb8ac761a7dfa6649f77c123be7a2b3c3d935a
Whitemind109 <ganada38@naver.com>
2025-12-26 23:01:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        int answer = x;
+        int sum = 0;
+
+        while(x != 0){
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if(answer % sum == 0) return true;
+        else return false;
+    }
+}
