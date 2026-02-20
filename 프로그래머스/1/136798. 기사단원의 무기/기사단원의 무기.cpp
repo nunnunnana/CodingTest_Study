08f30c66a07fcfe75e21c2bb9308e982442ff4d5
Whitemind109 <ganada38@naver.com>
2026-02-20 23:47:35 +0900

    [level 1] Title: 기사단원의 무기, Time: 57.69 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    for(int i = 1; i <= number; i++){
+        int count = 0;
+        for(int j = 1; j * j <= i; j++){
+            if(i % j == 0){
+                j * j == i ? count++ : count += 2;
+            }
+        }
+        answer = count > limit ? answer + power : answer + count;
+    }
+    return answer;
+}
