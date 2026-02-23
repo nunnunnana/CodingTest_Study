d038de929859d48fe5c4b4ac49ff5df89cfbcf13
Whitemind109 <ganada38@naver.com>
2026-02-23 20:11:52 +0900

    [level 1] Title: 모의고사, Time: 0.08 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int arr3[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    int count[3] = {0, 0, 0};
+    for(int i = 0; i < answers.size(); i++){
+        if(answers[i] == arr1[i % 5]) count[0]++;
+        if(answers[i] == arr2[i % 8]) count[1]++;
+        if(answers[i] == arr3[i % 10]) count[2]++;
+    }
+    
+    int max_val = count[0];
+    for (int i = 0; i < 3; i++) {
+        if (count[i] > max_val) {
+            max_val = count[i];
+        }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        if (count[i] == max_val) {
+            answer.push_back(i + 1);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
