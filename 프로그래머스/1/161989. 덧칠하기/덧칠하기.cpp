79f4318f806034e97184d51a462154de0af6f4a0
Whitemind109 <ganada38@naver.com>
2026-02-25 20:22:47 +0900

    [level 1] Title: 덧칠하기, Time: 0.24 ms, Memory: 5.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int solution(int n, int m, vector<int> section) {
-    int answer = 0;
-    int painted = 0;
+    int answer = 1;
+    int pivot = section[0];
     for(int i = 0; i < section.size(); i++){
-        if(painted < section[i]){
-            painted = section[i] + m - 1;
+        if(pivot + m > section[i]) continue;
+        else {
+            pivot = section[i];
             answer++;
         }
     }
